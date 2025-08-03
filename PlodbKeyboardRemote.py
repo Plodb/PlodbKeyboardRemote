8ad56f739e0c4e13edb33c5e1179d106e95462b1
user <plodb@tuta.io>
2025-08-04 01:59:56 +0200

    Huge bug fix

--- PlodbKeyboardRemote.py
+++ PlodbKeyboardRemote.py
@@ -24,10 +24,16 @@ import pystray
 from pystray import MenuItem as item
 from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 
-if sys.stderr is None:
-    logging.basicConfig(handlers=[logging.NullHandler()])
-else:
-    logging.basicConfig(level=logging.INFO)
+LOG_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "plodb.log")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(LOG_PATH, encoding="utf-8"),
+        logging.StreamHandler(sys.stderr) if sys.stderr else logging.NullHandler(),
+    ],
+)
 # --- Constants ---
 ICON_BASE64 = """/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAAyADIDASIAAhEBAxEB/8QAHAAAAQQDAQAAAAAAAAAAAAAAAAUGBwgCAwQB/8QALhAAAQMDAwMCBAcBAAAAAAAAAQIDBAAFEQYSIRMxQRRRByJhcRUyUmKBobHR/8QAGwEAAgMBAQEAAAAAAAAAAAAAAgMBBAUGAAf/xAApEQABAwMDAwIHAAAAAAAAAAABAAIDBBEhBRMxBkFhElEjMkKBkbHw/9oADAMBAAIRAxEAPwCvenbcLvfYNvU50kyHUtleM7QfNW3+HGjdLIjrYs1vjuTI6Qh185Lm4jg5UOxHmqv6J0bqXU0oO6cgPOdFYPqMhDaFDnlR4z9KtnpebedJ2BUrXlycmy1rwoxEKdbQnA5Xxwc+U4FZuvmKYCMzWI+kGxJ/uybcjK06ns06URGRbHgjJHCmgFJ8gnJx9D/VQJqLSDMqTMQYbcZ5gqUVIIRlPfPbCuCO2c+9WjkXA3W3tzbTIQ4laOo0oAKS5xwD7Cq/fEEypVvfUp9tu4KVtceI+XyDjaeM8c81kdP1rpJXxtOB73v9whPzDKgZ9KEvLS0ve2DhKsYyKwromQpEMj1DZSlX5Vd0q+x7Gueu+BBGESKKKKleV7NDT7BP0fbHdKttx7cWxtjpUCpo+Uq/dW+RHukhbjanorcZZO4FvqEp/Tg4HPnv3qj9iv8AdbDKRItM5+K4g7hsUdp+47GrN6H+Kb2pLCy+6mOiZHw3LQO+cjCwP0nzxxXzmv6RrXVBkpHNcScX7fm+fP6Saqpjp49yS9h7ZUmQbbGt9tbgwW+hHbGEJQcbec/7UV/ETpbChl5LMd0DslOSvcRkHtgdhx4pZueobmZRmRZCg0gZEfHy8Dnnz/NRRqrUr9xkPR3OnGkYwgJScq2jgg5wPP04qzpXSupafPu1ffPN8+VSp9Tp60fAN7eElSWm7napLSGSpS47ydrIyEuJVuQFEdvf/aiynhdr87bLe9bIeUvPKUXnCrOxJz8qRzjvk85pn12sEbmXutIYACKKKKsKVk2hTriUNpKlqOAAOTTh0bPNlu7ctcpttsgtvNFKlb0ZwQQBj7U32XXGHUOsrUhxBylSTgg1vXOedXukbXj3+Yc5+45oTfsoLWubZynrTuqYV3ivIjrX1EqThCs7zzxnx4xSLquItt/1bIaC8FCVODAbcPAIHge3vUZWC4+jll6PJ9K9zgL5Qfbnx/IpyXjU13/Dm/XQWhHeA6a0kFKgCfbOatvqhJFtycrDj0p1JU71Pb0nkJl3S3ToEhabhHeacKuVLSfmPfOfNcVKSrtMPU2urCFq3KQo7h/dYqxK3rVHSnJGVNJ2gcdsdvFU/URyt0WKT6KUxbI5AP4nEH0O7/lFRuNR7bkmUUUUxAinbYiRYnFgnekgJV5Az4oooXcKW8pR1I2hFpjrShKVqSjKgME8HuaYSlqV+ZRP3NFFAxMfwvKKKKalL//Z"""
 HTML_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "index.html")
@@ -367,8 +373,15 @@ if __name__ == "__main__":
     ws_thread.start()
     http_thread.start()
     start_key_watcher()
+
     try:
-        show_gui()
+
+        if sys.stdin and sys.stdout and sys.stderr:
+            show_gui()
+        else:
+
+            while not shutting_down:
+                time.sleep(0.5)
     finally:
         shutdown()
         ws_thread.join()
